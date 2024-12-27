dea1f20a80673457c19a1af26a3e67384737c45c
ramaxx95 <ramilozano@live.com.ar>
2024-12-27 17:20:45 -0300

    feat: clase abastracta para las piezas

---
+++ src/model/tetrimino/tetrimino.cpp
@@ -0,0 +1,22 @@
+#include "tetrimino.h"
+
+Tetrimino::Tetrimino() : x(0), y(0), width(0), height(0){}
+
+bool Tetrimino::isAnyBlockColliding(int x_thres, int y_thres){
+
+    int pos_x, pos_y = 0;
+    for(size_t i = 0; i < this->blocks.size(); i++){
+        if(this->blocks[i].lowerSideOccupied()){
+            continue;
+        }
+
+        this->blocks[i].getPosition(pos_x, pos_y);
+        if(pos_x == x_thres && 
+           pos_y == y_thres - 1){
+            return true;
+        }
+    }
+    return false;
+}
+
+Tetrimino::~Tetrimino(){}
---
+++ src/model/tetrimino/tetrimino.h
@@ -0,0 +1,40 @@
+#ifndef TETRIMINO_H
+#define TETRIMINO_H
+
+#include <vector>
+
+#include "block.h"
+
+// Clase abstracta de Tetriminos
+class Tetrimino{
+      
+
+    public:
+        // Posicion en el eje 'x' del bloque mas cercano al extremo izquierdo superior
+        int x;
+
+        // Posicion en el eje 'y' del bloque mas cercano al extremo izquierdo superior
+        int y;
+
+        // Ancho de la pieza
+        int width;
+
+        // Alto de la pieza
+        int height;
+
+        // Bloques que componen el Tetrimino
+        std::vector<Block> blocks;
+
+        /*** METODOS ***/
+
+        Tetrimino();
+
+        virtual int rotate() = 0;
+
+        bool isAnyBlockColliding(int x_thres, int y_thres);
+
+        virtual ~Tetrimino();
+
+};
+
+#endif //TETRIMINO_H
